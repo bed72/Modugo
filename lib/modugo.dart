2721b7eafa82b14e83abd4fee41edf964d7a1c09
Tech <gabriel.ramos@elo7.com>
2025-06-02 17:47:05 -0300

    :fire: exporte modugo

--- lib/modugo.dart
+++ lib/modugo.dart
@@ -1,5 +1,17 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/modugo.dart';
+export 'src/module.dart';
+export 'src/dispose.dart';
+export 'src/manager.dart';
+export 'src/injector.dart';
+
+export 'src/transitions/transition.dart';
+
+export 'src/routes/child_route.dart';
+export 'src/routes/module_route.dart';
+export 'src/routes/shell_module_route.dart';
+export 'src/routes/models/route_model.dart';
+
+export 'src/interfaces/module_interface.dart';
+export 'src/interfaces/manager_interface.dart';
+
+export 'src/extensions/context_extension.dart';
