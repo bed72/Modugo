fde9b4672a654a171050f9cd8db7a96f5b326b80
Tech <gabriel.ramos@elo7.com>
2025-06-05 22:06:46 -0300

    :fire: organize structure

--- lib/modugo.dart
+++ lib/modugo.dart
@@ -2,17 +2,15 @@ export 'src/modugo.dart';
 export 'src/module.dart';
 export 'src/dispose.dart';
 export 'src/manager.dart';
-
 export 'src/injector.dart';
-
+export 'src/extension.dart';
 export 'src/transition.dart';
+
+export 'src/interfaces/module_interface.dart';
+export 'src/interfaces/manager_interface.dart';
 
 export 'src/routes/child_route.dart';
 export 'src/routes/module_route.dart';
 export 'src/routes/shell_module_route.dart';
 export 'src/routes/models/route_model.dart';
-
-export 'src/interfaces/module_interface.dart';
-export 'src/interfaces/manager_interface.dart';
-
-export 'src/extensions/context_extension.dart';
+export 'src/routes/stateful_shell_module_route.dart';
