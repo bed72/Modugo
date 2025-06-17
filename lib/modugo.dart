a90b4f0e0a25b15b53d754b45bbd252afb62f9c5
Tech <gabriel.ramos@elo7.com>
2025-06-17 09:18:19 -0300

    :fire: atualiza exports

--- lib/modugo.dart
+++ lib/modugo.dart
@@ -6,8 +6,14 @@ export 'src/injector.dart';
 export 'src/extension.dart';
 export 'src/transition.dart';
 
+export 'src/binds/factory_bind.dart';
+export 'src/binds/singleton_bind.dart';
+export 'src/binds/lazy_singleton_bind.dart';
+
+export 'src/interfaces/bind_interface.dart';
 export 'src/interfaces/module_interface.dart';
 export 'src/interfaces/manager_interface.dart';
+export 'src/interfaces/injector_interface.dart';
 
 export 'src/routes/child_route.dart';
 export 'src/routes/module_route.dart';
